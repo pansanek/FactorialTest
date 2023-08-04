c1dfc6cc81181b16f86dc3adc6df3ddd58e3083c
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-04 14:42:13 +0300

    Создание приложения Factorial

--- app/src/main/java/ru/potemkin/factorialtest/MainActivity.kt
+++ app/src/main/java/ru/potemkin/factorialtest/MainActivity.kt
@@ -2,10 +2,51 @@ package ru.potemkin.factorialtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import ru.potemkin.factorialtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        observeViewModel()
+        binding.buttonCalculate.setOnClickListener {
+            viewModel.calculate(binding.editTextNumber.text.toString())
+        }
+    }
+
+    private fun observeViewModel(){
+        viewModel.progress.observe(this){
+            if(it){
+                binding.progressBarLoading.visibility= View.VISIBLE
+                binding.buttonCalculate.isEnabled = false
+            }else{
+                binding.progressBarLoading.visibility= View.INVISIBLE
+                binding.buttonCalculate.isEnabled = true
+            }
+        }
+        viewModel.error.observe(this){
+            if(it){
+                Toast.makeText(
+                    this,
+                    "No value given",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        viewModel.factorial.observe(this){
+            binding.textViewFactorial.text=it
+
+        }
+
     }
 }
