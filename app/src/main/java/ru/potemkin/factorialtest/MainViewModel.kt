4ea26da0365722d701b347097052d8d27d4db8d4
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-05 14:30:38 +0300

    CoroutineScope and CoroutineContext

--- app/src/main/java/ru/potemkin/factorialtest/MainViewModel.kt
+++ app/src/main/java/ru/potemkin/factorialtest/MainViewModel.kt
@@ -1,13 +1,11 @@
 package ru.potemkin.factorialtest
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 import java.math.BigInteger
 import kotlin.concurrent.thread
@@ -15,6 +13,8 @@ import kotlin.coroutines.suspendCoroutine
 
 class MainViewModel : ViewModel() {
 
+    private val coroutineScope =
+        CoroutineScope(Dispatchers.Main + CoroutineName("My coroutine scope"))
     private val _state = MutableLiveData<State>()
     val state: LiveData<State>
         get() = _state
@@ -25,22 +25,25 @@ class MainViewModel : ViewModel() {
             _state.value = Error
             return
         }
-        viewModelScope.launch {
+        coroutineScope.launch {
             val number = value.toLong()
-            val result = factorial(number)
+            val result = withContext(Dispatchers.Default) {
+                factorial(number)
+
+            }
             _state.value = Factorial(result)
+            Log.d("MainViewModel", coroutineContext.toString())
         }
     }
 
 
     private suspend fun factorial(number: Long): String {
-        return withContext(Dispatchers.Default) {
-            var result = BigInteger.ONE
-            for (i in 1..number) {
-                result = result.multiply(BigInteger.valueOf(i))
-            }
-            result.toString()
+        var result = BigInteger.ONE
+        for (i in 1..number) {
+            result = result.multiply(BigInteger.valueOf(i))
         }
+        return result.toString()
+
 
     }
 
@@ -54,4 +57,9 @@ class MainViewModel : ViewModel() {
 //        } }
 //
 //    }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+    }
 }
